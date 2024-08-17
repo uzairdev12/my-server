5fcec77c773bdb9d760f1568dadd9db38a32ce41
uzairdev12 <uzairmanan34@gmail.com>
2024-08-18 00:26:06 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   // Hardcoded response for '1' and '0'
-  const responseData = "1"; // Change this value as needed
+  const responseData = "0"; // Change this value as needed
   res.send(responseData);
 });
 
